78da73a20726ce2a997b8df57a045f0f03a8c77e
Alfonso Avila <jaagxd532@gmail.com>
2025-02-18 09:43:54 -0500

    act 1

---
+++ scrip.js
@@ -0,0 +1,8 @@
+/* crea una valiable llamada precio y asignale in valor decimal.
+luego, crea otra variable llamada cantidad y asignale un valor entero
+muestre tipo de variable en la consola*/
+
+let precio = 3.15;
+let cantidad = 5;
+console.log(typeof precio)
+console.log(typeof cantidad)
